c594287b363f5f95f8d84a7a26f8543fcacff758
Leelakrishna-Devops <114473726+Leelakrishna-Devops@users.noreply.github.com>
2022-10-03 21:02:37 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,2 +1,12 @@
 New python program file after force push
 
+class Main {
+  public static void main(String[] args) {
+    
+    // declare variables
+    int a = 12, b = 5;
+
+    // addition operator
+    System.out.println("a + b = " + (a + b));
+
+    // subtraction operator
